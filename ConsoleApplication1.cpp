00765799a59d8ba830d3c2df39a46fdef2e38bbe
BoeseKnight <71173444+BoeseKnight@users.noreply.github.com>
2020-10-08 18:20:54 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string.h>
+#include "Class.h"
+using namespace std;
+
+
+int main()
+{
+    serve a;
+    a.set_meat();
+    a.set_garnish();
+    a.set_desert();
+    a.cooking_meat();
+    a.set_sauce();
+    a.quantity();
+    a.cooking_garnish();
+    a.cooking_cream();
+    a.cooking_desert();
+    a.order();
+    a.rating();
+}
