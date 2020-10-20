4ea77c8100943756666dd19ad94d4a164120b0ae
BoeseKnight <71173444+BoeseKnight@users.noreply.github.com>
2020-10-20 21:50:01 +0300

    Update LAB6.cpp

--- LAB6.cpp
+++ LAB6.cpp
@@ -7,19 +7,42 @@ int search(T* array, T key, int size) {
         if (array[i] == key) {
             return i;
         }
-        
-    }
+    } 
+    return -1;
 }
 int main()
 {
-    int size, key,i=0;
+    int size;
+    /*int key, i = 0;
     cout << "Enter a size of array\n";
-    cin >> size;
+    bool fail = true;
+    do {
+
+        cin >> size;
+        if (cin.fail() || cin.rdbuf()->in_avail() > 1) {
+            cout << "Error" << endl;
+        }
+        else
+            fail = false;
+        cin.clear();
+        cin.ignore(cin.rdbuf()->in_avail());
+    } while (fail);
     int *arr=new int[size];
-    cout << "Enter a key to find\n";
-    cin >> key;
+    cout << "Enter a key to find\n"; 
+    fail = true;
+    do {
+       
+        cin >> key;
+        if (cin.fail() || cin.rdbuf()->in_avail() > 1) {
+            cout << "Error" << endl;
+        }
+        else
+            fail = false;
+        cin.clear();
+        cin.ignore(cin.rdbuf()->in_avail());
+    } while (fail);
     for (i = 0; i < size; i++) {
-        bool fail = true;
+        fail = true;
         do {
             cin >> arr[i];
             if (cin.fail() || cin.rdbuf()->in_avail() > 1){
@@ -31,5 +54,29 @@ int main()
             cin.ignore(cin.rdbuf()->in_avail());
         } while (fail);
     }
-    cout<<"The index of the found element is "<<search<int>(arr, key, size)<<" and it is equal key="<<arr[search<int>(arr, key, size)];
+    if (search<int>(arr, key, size) != -1)
+        cout << "The index of the found element is " << search<int>(arr, key, size) << " and it is equal key=" << arr[search<int>(arr, key, size)];
+    else cout << "There is no such element((";*/
+    cout << "Enter a size of string\n";
+    bool fail = true;
+    do {
+        cin >> size;
+        if (cin.fail() || cin.rdbuf()->in_avail() > 1) {
+            cout << "Error" << endl;
+        }
+        else
+            fail = false;
+        cin.clear();
+        cin.ignore(cin.rdbuf()->in_avail());
+    } while (fail);
+    char* str = new char[size];
+    char key1;
+    cout << "Enter a key to find\n";
+    cin >> key1;
+    cin.ignore();
+    cin.getline(str, size);
+    search<char>(str, key1, size);
+    if (search<char>(str, key1, size) != -1)
+        cout << "The index of the found element is " << search<char>(str, key1, size) << " and it is equal key=" << str[search<char>(str, key1, size)];
+    else cout << "There is no such element((";
 }
